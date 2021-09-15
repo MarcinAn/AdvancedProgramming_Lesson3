727184b25963329e9239aa008b200ad45bd8fe88
Marcin Antonik <marcin.antonik91@gmail.com>
2021-09-15 17:17:27 +0200

    Add project files.

---
+++ AdvancedProgramming_Lesson3/Models/OsobaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvancedProgramming_Lesson3.Models
+{
+    public class OsobaContext : DbContext
+    {
+        public OsobaContext(DbContextOptions<OsobaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<OsobaItem> osobaItems { get; set; }
+    }
+}
